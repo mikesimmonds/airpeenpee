040418a58e516eb29c87cb6c5d19cb95511d8548
MichaelSimmonds <mrmichaelsimmonds@gmail.com>
2017-03-28 14:10:24 +0200

    Controlers complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+
+  get 'open_times/index'
+
+  get 'open_times/show'
+
+  get 'open_times/new'
+
+  get 'open_times/create'
+
+  get 'open_times/edit'
+
+  get 'open_times/update'
+
+  get 'open_times/delete'
+
+  resources :users, :toilets, :visits
+
   devise_for :accounts
   devise_for :installs
   root to: 'pages#home'
