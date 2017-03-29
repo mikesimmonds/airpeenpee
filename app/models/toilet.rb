5d560eb7e9a9b1b742083b30185baa382c34c085
Friso Jelsma <friso.jelsma@gmail.com>
2017-03-29 16:43:10 +0200

    added seed file

--- app/models/toilet.rb
+++ app/models/toilet.rb
@@ -2,6 +2,6 @@ class Toilet < ApplicationRecord
   belongs_to :user
   has_one :open_time
 
-  geocoded_by :address
-  after_validation :geocode, if: :address_changed?
+  geocoded_by :location_address
+  after_validation :geocode, if: :location_address_changed?
 end
