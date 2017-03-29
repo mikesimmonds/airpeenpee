fb81b68197d28eeb0121692a83283ca671de3dae
Jasper LeponisDean <j.leponis.dean@gmail.com>
2017-03-29 16:51:46 +0200

    Added fading buttons to banner

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,18 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require_tree .
+
+$( document ).ready(function(){
+
+  // Pee and Poo buttons
+
+  $('#pee-btn, #poo-btn').click(function(){
+    $('.banner-content > h1, p, #pee-btn, #poo-btn').fadeOut(2000, function(){
+      $('#toilet-list').fadeIn(2000).removeClass('hidden');
+    });
+  });
+
+
+
+
+});
