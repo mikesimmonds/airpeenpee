5a15add50d6959ac4e7b9fd1655bd760002e261f
Jasper LeponisDean <j.leponis.dean@gmail.com>
2017-03-30 15:39:27 +0200

    Implemented basic features on home and index

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,17 +3,3 @@
 //= require bootstrap-sprockets
 //= require_tree .
 
-$( document ).ready(function(){
-
-  // Pee and Poo buttons
-
-  $('#pee-btn, #poo-btn').click(function(){
-    $('.banner-content > h1, p, #pee-btn, #poo-btn').fadeOut(2000, function(){
-      $('#toilet-list').fadeIn(2000).removeClass('hidden');
-    });
-  });
-
-
-
-
-});
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
 
   devise_for :accounts, :controllers => { registrations: "registrations"}
 
-  root to: 'toilets#index'
+  root to: 'toilets#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
