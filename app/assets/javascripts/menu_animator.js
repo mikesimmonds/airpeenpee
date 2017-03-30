0795d80f64c05e35dcae27db45d7788c15557e5f
MichaelSimmonds <mrmichaelsimmonds@gmail.com>
2017-03-30 22:21:07 +0200

    styles the forms

---
+++ app/assets/javascripts/menu_animator.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+  $('.form-container').animate({top: '10px'},"fast");
+  $('.form-container').animate({top: '-10px'},"fast");
+  // $('.form-container').animate({top: '5px'},"fast");
+  // $('.form-container').animate({top: '-5px'},"fast");
+});
